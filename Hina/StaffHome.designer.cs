2fc268235f78a97fda4abd7fcb5ce5d0862ca682
Nurun Nisa YILMAZ <nurun.yilmaz@ogr.sakarya.edu.tr>
2019-12-16 02:41:53 +0300

    staff home setting

---
+++ Hina/StaffHome.designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Hina
+{
+    partial class StaffHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StaffHome));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pctrbxHakkinda = new System.Windows.Forms.PictureBox();
+            this.pctrbxAyarlar = new System.Windows.Forms.PictureBox();
+            this.pctrbxGecikmisKitaplar = new System.Windows.Forms.PictureBox();
+            this.pctrbxUyeIslemleri = new System.Windows.Forms.PictureBox();
+            this.pctrbxEmanetIslemleri = new System.Windows.Forms.PictureBox();
+            this.pctrbxKitapIslemleri = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxHakkinda)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxAyarlar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxGecikmisKitaplar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxUyeIslemleri)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxEmanetIslemleri)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxKitapIslemleri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.pctrbxHakkinda);
+            this.groupBox1.Controls.Add(this.pctrbxAyarlar);
+            this.groupBox1.Controls.Add(this.pctrbxGecikmisKitaplar);
+            this.groupBox1.Controls.Add(this.pctrbxUyeIslemleri);
+            this.groupBox1.Controls.Add(this.pctrbxEmanetIslemleri);
+            this.groupBox1.Controls.Add(this.pctrbxKitapIslemleri);
+            this.groupBox1.Location = new System.Drawing.Point(55, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(653, 372);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(490, 295);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(64, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Hakkında";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(298, 295);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ayarlar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(92, 295);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Gecikmiş Kitaplar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(462, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Emanet İşlemleri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(279, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 18);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Üye İşlemleri";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(92, 140);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Kitap İşlemleri";
+            // 
+            // pctrbxHakkinda
+            // 
+            this.pctrbxHakkinda.Image = ((System.Drawing.Image)(resources.GetObject("pctrbxHakkinda.Image")));
+            this.pctrbxHakkinda.Location = new System.Drawing.Point(487, 219);
+            this.pctrbxHakkinda.Name = "pctrbxHakkinda";
+            this.pctrbxHakkinda.Size = new System.Drawing.Size(60, 60);
+            this.pctrbxHakkinda.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrbxHakkinda.TabIndex = 5;
+            this.pctrbxHakkinda.TabStop = false;
+            // 
+            // pctrbxAyarlar
+            // 
+            this.pctrbxAyarlar.Image = ((System.Drawing.Image)(resources.GetObject("pctrbxAyarlar.Image")));
+            this.pctrbxAyarlar.Location = new System.Drawing.Point(295, 219);
+            this.pctrbxAyarlar.Name = "pctrbxAyarlar";
+            this.pctrbxAyarlar.Size = new System.Drawing.Size(60, 60);
+            this.pctrbxAyarlar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrbxAyarlar.TabIndex = 4;
+            this.pctrbxAyarlar.TabStop = false;
+            // 
+            // pctrbxGecikmisKitaplar
+            // 
+            this.pctrbxGecikmisKitaplar.Image = ((System.Drawing.Image)(resources.GetObject("pctrbxGecikmisKitaplar.Image")));
+            this.pctrbxGecikmisKitaplar.Location = new System.Drawing.Point(112, 219);
+            this.pctrbxGecikmisKitaplar.Name = "pctrbxGecikmisKitaplar";
+            this.pctrbxGecikmisKitaplar.Size = new System.Drawing.Size(60, 60);
+            this.pctrbxGecikmisKitaplar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrbxGecikmisKitaplar.TabIndex = 3;
+            this.pctrbxGecikmisKitaplar.TabStop = false;
+            // 
+            // pctrbxUyeIslemleri
+            // 
+            this.pctrbxUyeIslemleri.Image = ((System.Drawing.Image)(resources.GetObject("pctrbxUyeIslemleri.Image")));
+            this.pctrbxUyeIslemleri.Location = new System.Drawing.Point(295, 73);
+            this.pctrbxUyeIslemleri.Name = "pctrbxUyeIslemleri";
+            this.pctrbxUyeIslemleri.Size = new System.Drawing.Size(60, 60);
+            this.pctrbxUyeIslemleri.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrbxUyeIslemleri.TabIndex = 2;
+            this.pctrbxUyeIslemleri.TabStop = false;
+            // 
+            // pctrbxEmanetIslemleri
+            // 
+            this.pctrbxEmanetIslemleri.Image = ((System.Drawing.Image)(resources.GetObject("pctrbxEmanetIslemleri.Image")));
+            this.pctrbxEmanetIslemleri.Location = new System.Drawing.Point(487, 73);
+            this.pctrbxEmanetIslemleri.Name = "pctrbxEmanetIslemleri";
+            this.pctrbxEmanetIslemleri.Size = new System.Drawing.Size(60, 60);
+            this.pctrbxEmanetIslemleri.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrbxEmanetIslemleri.TabIndex = 1;
+            this.pctrbxEmanetIslemleri.TabStop = false;
+            // 
+            // pctrbxKitapIslemleri
+            // 
+            this.pctrbxKitapIslemleri.Image = ((System.Drawing.Image)(resources.GetObject("pctrbxKitapIslemleri.Image")));
+            this.pctrbxKitapIslemleri.Location = new System.Drawing.Point(112, 73);
+            this.pctrbxKitapIslemleri.Name = "pctrbxKitapIslemleri";
+            this.pctrbxKitapIslemleri.Size = new System.Drawing.Size(60, 60);
+            this.pctrbxKitapIslemleri.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctrbxKitapIslemleri.TabIndex = 0;
+            this.pctrbxKitapIslemleri.TabStop = false;
+            // 
+            // StaffHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 450);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "StaffHome";
+            this.Text = "StaffHome";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxHakkinda)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxAyarlar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxGecikmisKitaplar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxUyeIslemleri)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxEmanetIslemleri)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctrbxKitapIslemleri)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pctrbxHakkinda;
+        private System.Windows.Forms.PictureBox pctrbxAyarlar;
+        private System.Windows.Forms.PictureBox pctrbxGecikmisKitaplar;
+        private System.Windows.Forms.PictureBox pctrbxUyeIslemleri;
+        private System.Windows.Forms.PictureBox pctrbxEmanetIslemleri;
+        private System.Windows.Forms.PictureBox pctrbxKitapIslemleri;
+    }
+}
