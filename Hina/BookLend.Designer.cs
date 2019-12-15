cc08e53413424deb065d3561a086aff1d6966090
Nurun Nisa YILMAZ <nurun.yilmaz@ogr.sakarya.edu.tr>
2019-12-15 18:02:16 +0300

    Book lend page designer

--- Hina/BookLend.Designer.cs
+++ Hina/BookLend.Designer.cs
@@ -28,22 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtbTcKimlik = new System.Windows.Forms.TextBox();
+            this.txtbKitapKodu = new System.Windows.Forms.TextBox();
+            this.txtbAlimTarihi = new System.Windows.Forms.TextBox();
+            this.txtbTeslimTarihi = new System.Windows.Forms.TextBox();
+            this.bttnKaydet = new System.Windows.Forms.Button();
+            this.bttnIptal = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 57);
+            this.label1.Location = new System.Drawing.Point(29, 89);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 0;
@@ -52,7 +57,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 93);
+            this.label2.Location = new System.Drawing.Point(29, 125);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(59, 13);
             this.label2.TabIndex = 1;
@@ -61,7 +66,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(231, 57);
+            this.label3.Location = new System.Drawing.Point(229, 89);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(55, 13);
             this.label3.TabIndex = 2;
@@ -70,75 +75,99 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(231, 96);
+            this.label4.Location = new System.Drawing.Point(229, 128);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(66, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Teslim Tarihi";
             // 
-            // textBox1
+            // txtbTcKimlik
             // 
-            this.textBox1.Location = new System.Drawing.Point(88, 54);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 4;
+            this.txtbTcKimlik.Location = new System.Drawing.Point(86, 86);
+            this.txtbTcKimlik.Name = "txtbTcKimlik";
+            this.txtbTcKimlik.Size = new System.Drawing.Size(100, 20);
+            this.txtbTcKimlik.TabIndex = 4;
             // 
-            // textBox2
+            // txtbKitapKodu
             // 
-            this.textBox2.Location = new System.Drawing.Point(88, 93);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 5;
+            this.txtbKitapKodu.Location = new System.Drawing.Point(86, 125);
+            this.txtbKitapKodu.Name = "txtbKitapKodu";
+            this.txtbKitapKodu.Size = new System.Drawing.Size(100, 20);
+            this.txtbKitapKodu.TabIndex = 5;
             // 
-            // textBox3
+            // txtbAlimTarihi
             // 
-            this.textBox3.Location = new System.Drawing.Point(301, 54);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 6;
+            this.txtbAlimTarihi.Location = new System.Drawing.Point(299, 86);
+            this.txtbAlimTarihi.Name = "txtbAlimTarihi";
+            this.txtbAlimTarihi.Size = new System.Drawing.Size(100, 20);
+            this.txtbAlimTarihi.TabIndex = 6;
             // 
-            // textBox4
+            // txtbTeslimTarihi
             // 
-            this.textBox4.Location = new System.Drawing.Point(301, 93);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
-            this.textBox4.TabIndex = 7;
+            this.txtbTeslimTarihi.Location = new System.Drawing.Point(299, 125);
+            this.txtbTeslimTarihi.Name = "txtbTeslimTarihi";
+            this.txtbTeslimTarihi.Size = new System.Drawing.Size(100, 20);
+            this.txtbTeslimTarihi.TabIndex = 7;
             // 
-            // button1
+            // bttnKaydet
             // 
-            this.button1.Location = new System.Drawing.Point(113, 175);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Kaydet";
-            this.button1.UseVisualStyleBackColor = true;
+            this.bttnKaydet.Location = new System.Drawing.Point(111, 207);
+            this.bttnKaydet.Name = "bttnKaydet";
+            this.bttnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.bttnKaydet.TabIndex = 8;
+            this.bttnKaydet.Text = "Kaydet";
+            this.bttnKaydet.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // bttnIptal
             // 
-            this.button2.Location = new System.Drawing.Point(243, 175);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "İptal";
-            this.button2.UseVisualStyleBackColor = true;
+            this.bttnIptal.Location = new System.Drawing.Point(241, 207);
+            this.bttnIptal.Name = "bttnIptal";
+            this.bttnIptal.Size = new System.Drawing.Size(75, 23);
+            this.bttnIptal.TabIndex = 9;
+            this.bttnIptal.Text = "İptal";
+            this.bttnIptal.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(28, 30);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(392, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 30);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(433, 363);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bttnIptal);
+            this.Controls.Add(this.bttnKaydet);
+            this.Controls.Add(this.txtbTeslimTarihi);
+            this.Controls.Add(this.txtbAlimTarihi);
+            this.Controls.Add(this.txtbKitapKodu);
+            this.Controls.Add(this.txtbTcKimlik);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -150,12 +179,14 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtbTcKimlik;
+        private System.Windows.Forms.TextBox txtbKitapKodu;
+        private System.Windows.Forms.TextBox txtbAlimTarihi;
+        private System.Windows.Forms.TextBox txtbTeslimTarihi;
+        private System.Windows.Forms.Button bttnKaydet;
+        private System.Windows.Forms.Button bttnIptal;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
